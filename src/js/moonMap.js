23b55d7d92737ba1b0bb0cbbed66cc08cec25973
Roman <romanjeanelie@yahoo.fr>
2021-03-05 15:34:22 +0100

    first commit

---
+++ src/js/moonMap.js
@@ -0,0 +1,20 @@
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+
+// export default class MoonMap {
+//   constructor() {
+//     gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
+//     this.init();
+//   }
+
+//   init() {
+//     this.anim();
+//   }
+
+//   anim() {
+//     // gsap.from(".cls-10", { drawSVG: 0, duration: 5 });
+//     // gsap.from(".cls-11", { drawSVG: 0, duration: 5 });
+//     // gsap.from(".cls-12", { drawSVG: 0, duration: 5 });
+//   }
+// }
