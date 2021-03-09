4e3cf86c41679e1bedc45fff4918a395c3aaa149
Roman <romanjeanelie@yahoo.fr>
2021-03-09 11:59:59 +0100

    fix svg change ID

--- src/js/lines.js
+++ src/js/lines.js
@@ -22,22 +22,22 @@ export default class Lines {
     this.anim();
   }
 
-  allSvgLoaded() {
-    const allSvg = document.querySelectorAll("svg");
-    let index = 0;
-    console.log(allSvg);
+  // allSvgLoaded() {
+  //   const allSvg = document.querySelectorAll("svg");
+  //   let index = 0;
+  //   console.log(allSvg);
 
-    allSvg.forEach((svg) => {
-      svg.addEventListener("load", () => {
-        index++;
-        console.log("loading");
-        if (index === allSvg.length) {
-          this.anim();
-          console.log("loaded !");
-        }
-      });
-    });
-  }
+  //   allSvg.forEach((svg) => {
+  //     svg.addEventListener("load", () => {
+  //       index++;
+  //       console.log("loading");
+  //       if (index === allSvg.length) {
+  //         this.anim();
+  //         console.log("loaded !");
+  //       }
+  //     });
+  //   });
+  // }
 
   anim() {
     gsap.from("#intro-1", { scrollTrigger: "#intro-1", opacity: 0, delay: 0.5, duration: 2 });
@@ -45,23 +45,23 @@ export default class Lines {
 
     // gsap.set("#line-1", { attr: { fill: "none", stroke: "red", "stroke-width": 1 } });
     this.tl1.from("#moon", { opacity: 0, delay: 1, duration: 4 });
-    this.tl1.from("#line-1", { drawSVG: 0, duration: 5 }, "-=1");
+    this.tl1.from("line", { drawSVG: 0, duration: 5 }, "-=1");
     this.tl1.from("#text-1", { opacity: 0, duration: 5 }, "-=1");
 
     // // draw lines
 
     // setting
 
-    gsap.from("#line-2", { scrollTrigger: "#line-2", drawSVG: 0, duration: 5, delay: 4.5 });
-    gsap.from("#line-3", { scrollTrigger: "#line-3", drawSVG: 0, duration: 5, delay: 4.5 });
-    gsap.from("#line-4", { scrollTrigger: "#line-4", drawSVG: 0, duration: 5, delay: 4.5 });
-    gsap.from("#line-5", { scrollTrigger: "#line-5", drawSVG: 0, duration: 5, delay: 4.5 });
+    // gsap.from("#line-2", { scrollTrigger: "#line-2", drawSVG: 0, duration: 5, delay: 4.5 });
+    // gsap.from("#line-3", { scrollTrigger: "#line-3", drawSVG: 0, duration: 5, delay: 4.5 });
+    // gsap.from("#line-4", { scrollTrigger: "#line-4", drawSVG: 0, duration: 5, delay: 4.5 });
+    // gsap.from("#line-5", { scrollTrigger: "#line-5", drawSVG: 0, duration: 5, delay: 4.5 });
 
-    // // draw text
-    gsap.from("#text-2", { scrollTrigger: "#text-2", opacity: 0, duration: 5, delay: 2.5 });
-    gsap.from("#text-3", { scrollTrigger: "#text-3", opacity: 0, duration: 5, delay: 2.5 });
-    gsap.from("#text-4", { scrollTrigger: "#text-4", opacity: 0, duration: 5, delay: 2.5 });
-    gsap.from("#text-5", { scrollTrigger: "#text-5", opacity: 0, duration: 5, delay: 2.5 });
+    // // // draw text
+    // gsap.from("#text-2", { scrollTrigger: "#text-2", opacity: 0, duration: 5, delay: 2.5 });
+    // gsap.from("#text-3", { scrollTrigger: "#text-3", opacity: 0, duration: 5, delay: 2.5 });
+    // gsap.from("#text-4", { scrollTrigger: "#text-4", opacity: 0, duration: 5, delay: 2.5 });
+    // gsap.from("#text-5", { scrollTrigger: "#text-5", opacity: 0, duration: 5, delay: 2.5 });
 
     // To make moving down the moon
     // gsap.to("#moon-container", { scrollTrigger: "#text-5", y: "200vh", opacity: 1, duration: 15, ease: "Power3.out" });
